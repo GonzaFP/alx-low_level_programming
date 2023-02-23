c72c638cc067532722e30cbacf218117c3d691e5
root <fionagonza2@gmail.com>
2023-02-22 19:38:09 -0800

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+# include "main.h"
+/**
+ * add - adds 2 intergers
+ * @num1: first number
+ * @num2: second number
+ * Return: 0
+ */
+int add(int num1, int num2)
+{
+	int addition = num1 + num2;
+
+	return (addition);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+# include "main.h"
+/**
+ * times_table - prints time table from 0 to 9
+ * @void: void
+ * Return:0
+ */
+void times_table(void)
+{
+	int x = 0;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; x = x + 0)
+		{
+			_putchar(x + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+
+	}
+}
