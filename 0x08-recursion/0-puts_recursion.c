679713e55f0218172821ab391c76c06b4a06c3e8
root <fionagonza2@gmail.com>
2023-03-09 00:15:15 -0800

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
+ * return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[i]);
+	_puts_recursion(s + 1);
+}
